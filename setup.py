eda859d15a462f5c46c01a6a50a81e2534ab0399
Saurabh Yewale <yewalesaurabh2@gmail.com>
2025-03-03 21:25:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Saurabh Yewale',
+    author_email= 'yewalesaurabh2@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
